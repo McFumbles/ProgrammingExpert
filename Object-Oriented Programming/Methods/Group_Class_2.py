3bbe28b3adb87f0303747f4d06b3aefecec6844d
Maverick <tayqinyuan@gmail.com>
2022-02-01 11:53:49 +0000

    finished group class 2

---
+++ Object-Oriented Programming/Methods/Group_Class_2.py
@@ -0,0 +1,21 @@
+class Group:
+    def __init__(self, name, members=[]):
+        self.name = name
+        self.members = members
+
+    def add(self, name):
+        self.members.append(name)
+
+    def delete(self, name):
+        if name in self.members:
+            self.members.remove(name)
+        else:
+            raise Exception("Member not in group.")
+
+    def get_members(self):
+        return sorted(self.members)
+
+    def merge(self, group):
+      combined = self.members + group.members
+      new_group = Group("Any name", combined)
+      return new_group
